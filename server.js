84c084c800ef63733df90020fa4e99cd4ec353e4
Blake Hagen <blakehagen@gmail.com>
2015-12-01 11:18:52 -0700

    requireAuth

--- server.js
+++ server.js
@@ -9,15 +9,6 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// FACEBOOK STRATEGY //
-passport.use(new FacebookStrategy({
-    clientID: '991918067535238',
-    clientSecret: '41cc9fe52a3a581c0a08a96e763e38c9',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
-}, function (token, refreshToken, profile, done) {
-    return done(null, profile);
-}));
-
 app.use(session({
     secret: 'thisisasecret',
     resave: false,
@@ -26,6 +17,15 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+// FACEBOOK STRATEGY //
+passport.use(new FacebookStrategy({
+    clientID: '991918067535238',
+    clientSecret: '41cc9fe52a3a581c0a08a96e763e38c9',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+}, function (token, refreshToken, profile, done) {
+    return done(null, profile);
+}));
 
 passport.serializeUser(function (user, done) {
     console.log('user ' + user);
@@ -37,6 +37,13 @@ passport.deserializeUser(function (obj, done) {
     done(null, obj);
 })
 
+var requireAuth = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    return res.redirect('/login');
+};
+
 // AUTH ENDPOINTS //
 app.get('/auth/facebook', passport.authenticate('facebook'));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', {
@@ -45,7 +52,7 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook', {
 }))
 
 // MY INFO ENDPOINT //
-app.get('/api/me', function (req, res, next) {
+app.get('/api/me', requireAuth, function (req, res, next) {
     console.log('who dis? ' + req.user);
     res.json(req.user);
 });
